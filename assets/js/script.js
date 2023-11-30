a554367002e8a5cb9fd88bbee5d8d75c0f14e134
Ed Nowak <ed@7pointsolutions.com>
2023-11-29 17:46:16 -0700

    added error handling to API calls and refactored some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,71 @@
 const zipCodeEl = document.getElementById('zip-code');
 const zoneIdEl = document.getElementById('zone-id');
-const cardsContainerEl = document.getElementById('#cardsSection .columns');
+const cardsContainerEl = document.querySelector('#cardsSection .columns');
 
-//TODO: Remove defaults before go-live
-var zipCode = '80525';
-var zoneId = 5;
+var zipCode;
+var zoneId;
+var bypassAPIMode = true; //Bypasses Hardiness Zone API call
+
+// *****Bulma*****
+//Modal for Zone Map in Navbar
+document.addEventListener('DOMContentLoaded', () => {
+	// Get the modal and the button that opens the modal
+	const modal = document.getElementById('modal-hardiness-zone');
+	const btnOpenModal = document.getElementById('open-modal-hardiness-zone');
+	// Open the modal when the button is clicked
+	btnOpenModal.addEventListener('click', function() {
+			modal.classList.add('is-active');
+	});
+	// Close the modal when the close button is clicked or when the background is clicked
+	modal.querySelector('.modal-close').addEventListener('click', function() {
+			modal.classList.remove('is-active');
+	});
+	modal.querySelector('.modal-background').addEventListener('click', function() {
+			modal.classList.remove('is-active');
+	});
+
+
+// Modal for Zones Info
+const modalWhyZonesMatter = document.getElementById('modal-why-zones-matter');
+const btnOpenModalWhyZonesMatter = document.getElementById('open-modal-why-zones-matter');
+
+btnOpenModalWhyZonesMatter.addEventListener('click', function() {
+modalWhyZonesMatter.classList.add('is-active');
+});
+
+modalWhyZonesMatter.querySelector('.modal-close').addEventListener('click', function() {
+modalWhyZonesMatter.classList.remove('is-active');
+});
+});
+// *****End Bulma*****
 
 function initializePage() {
 	getLocalStorageLocation();
 	setPageLocation();
-}
+};
 
-// TODO: For testing only!  Get rid of this once wired-up to Bulma
-function bulmaFake() {
-	//TODO: Add zip code validation
+// TODO: This was used for prototyping.  Delete once Aloys wires-up new button!
+function oldButton() {
 	zipCode = prompt('Enter Zip Code', 80525);
-	zoneId = 5;
+	getPlants();
+};
 
-	/*
-	TODO: KEEP THIS!  Uncomment before go-live!
-	apiGetZoneId();
-	*/
+function getPlants() {
+	if (!zipCode) return;
+	if (bypassAPIMode) {
+			zoneId = '5';
+			setLocalStorageLocation();
+			setPageLocation();
+			console.log("Calling Plant API");
+			apiGetPlants();
+	} else {
+			console.log("Calling Hardiness Zone API");
+			apiGetZoneId();
+	}
+};
 
-	//TODO: Delete 2 lines below before go-live!
-	setLocalStorageLocation();
-	setPageLocation();
-}
-
-function apiGetZoneId() {
-	// TODO: Add error handling
-	fetch('https://plant-hardiness-zone.p.rapidapi.com/zipcodes/' + zipCode, {
+async function apiGetZoneId() {
+	await fetch('https://plant-hardiness-zone.p.rapidapi.com/zipcodes/' + zipCode, {
 		method: 'GET',
 		headers: {
 			'X-RapidAPI-Key': '54396a694cmshf03550fa50210dbp190d85jsn85ec9ece51f3',
@@ -37,23 +73,32 @@ function apiGetZoneId() {
 		}
 	})
 	.then(function (response) {
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
 		return response.json();
 	})
 	.then(function (data) {
 		zoneId = data.hardiness_zone;
 		setLocalStorageLocation();
 		setPageLocation();
-});
+		apiGetPlants();
+	})
+	.catch(function (error) {
+		console.error('Hardiness Zone API request error:', error);
+		alert('An unexpected error was encountered trying to process your zone ID request.  Please try again.');
+	});
 }
 
 function apiGetPlants() {
-	// TODO: Add error handling
 	fetch('https://perenual.com/api/species-list?key=sk-2rfU6564bcc049f6a3151&edible=1&edible=1&hardiness=' + zoneId)
 	.then(function (response) {
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
 		return response.json();
 	})
 	.then(function (data) {
-		const cardsContainerEl = document.querySelector('#cardsSection .columns');
 		cardsContainerEl.innerHTML='';
 		for (let i = 0; i < data.data.length; i++) {
 			//Creating columns so the cards dont stack
@@ -129,23 +174,32 @@ function apiGetPlants() {
 			// Append main card div to card-container
 			cardsContainerEl.appendChild(columnDiv);
 		}
-});
-}
-
-function setLocalStorageLocation() {
-	localStorage.setItem("zipCode", zipCode);
-	zoneId = removeLetters(zoneId);
-	localStorage.setItem("zoneId", zoneId);
+})
+	.catch(function (error) {
+		console.error('Plant API request error:', error);
+		alert('An unexpected error was encountered trying to process your plant listing.  Please try again.');
+		return 0;
+	});
 }
 
 function getLocalStorageLocation() {
 	zipCode = localStorage.getItem("zipCode");
+	if (!zipCode) {zipCode = 0}
 	zoneId = localStorage.getItem("zoneId");
+	if (!zoneId) {zoneId = 0}
 }
 
 function setPageLocation() {
 	zipCodeEl.textContent = zipCode;
 	zoneIdEl.textContent = zoneId;
+}
+
+function setLocalStorageLocation() {
+	localStorage.setItem("zipCode", zipCode);
+	if (typeof zoneId === 'string') {
+		zoneId = removeLetters(zoneId);
+	}
+	localStorage.setItem("zoneId", zoneId);
 }
 
 // The Zone Id returned from the Plant Hardiness Zone API can include a letter.  The Perenual API only accepts an integer.
@@ -155,35 +209,4 @@ function removeLetters(zone) {
 }
 
 initializePage();
-
-//Modal for Zone Map in Navbar
-document.addEventListener('DOMContentLoaded', () => {
-    // Get the modal and the button that opens the modal
-    const modal = document.getElementById('modal-hardiness-zone');
-    const btnOpenModal = document.getElementById('open-modal-hardiness-zone');
-    // Open the modal when the button is clicked
-    btnOpenModal.addEventListener('click', function() {
-        modal.classList.add('is-active');
-    });
-    // Close the modal when the close button is clicked or when the background is clicked
-    modal.querySelector('.modal-close').addEventListener('click', function() {
-        modal.classList.remove('is-active');
-    });
-    modal.querySelector('.modal-background').addEventListener('click', function() {
-        modal.classList.remove('is-active');
-    });
-
-
-// Modal for Zones Info
-const modalWhyZonesMatter = document.getElementById('modal-why-zones-matter');
-const btnOpenModalWhyZonesMatter = document.getElementById('open-modal-why-zones-matter');
-
-btnOpenModalWhyZonesMatter.addEventListener('click', function() {
-	modalWhyZonesMatter.classList.add('is-active');
-});
-
-modalWhyZonesMatter.querySelector('.modal-close').addEventListener('click', function() {
-	modalWhyZonesMatter.classList.remove('is-active');
-});
-});
 	
